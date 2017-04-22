6b08e22534d2d1738c603c4e2fa9ce1327490bbc
rubonics <rubenrodriguez+github@gmail.com>
2017-04-21 22:50:00 -0400

    adds controls referece to cms.type.item

--- libs/swig_functions.js
+++ libs/swig_functions.js
@@ -186,6 +186,7 @@ module.exports.swigFunctions = function(swig) {
 
     item._type = type;
     item._id = key;
+    item._controls = self.typeInfo[type].controls;
 
     if(!item.slug) {
       var tmpSlug = generateSlug(item);
